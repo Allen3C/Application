dc0783ec8fe0ac6290f18b7583c593991ddeda1d
Allen3C <allen3c@163.com>
2020-04-18 11:19:57 +0800

    协调布局： CoordinatorLayout、AppBarLayout、CollapsingToolbarLayout、Toobar

--- app/src/main/java/com/example/application/main/shanghai/ShangHaiFragment.java
+++ app/src/main/java/com/example/application/main/shanghai/ShangHaiFragment.java
@@ -1,15 +1,42 @@
 package com.example.application.main.shanghai;
 
-import androidx.fragment.app.Fragment;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.application.R;
 import com.example.application.base.BaseFragment;
 import com.example.application.base.ViewInject;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import butterknife.BindView;
 
 @ViewInject(mainlayoutid = R.layout.fragment_shanghai)
 public class ShangHaiFragment extends BaseFragment {
+
+
+    @BindView(R.id.tv_shanghai_welcome)
+    TextView tvShanghaiWelcome;
+    @BindView(R.id.shanghai_collapsingtoolbarlayout)
+    CollapsingToolbarLayout shanghaiCollapsingtoolbarlayout;
+    @BindView(R.id.shanghai_app_barlayout)
+    AppBarLayout shanghaiAppBarlayout;
+
     @Override
     public void afterBindView() {
+        shanghaiAppBarlayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            //两个参数：AppBarLayout对象本身， 滑动的距离
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                Log.e("shanghaiAppBarlayout","verticalOffset = " + verticalOffset + "appBarLayout = " + appBarLayout.getMeasuredHeight());
+                if(-verticalOffset < appBarLayout.getMeasuredHeight() / 2){
+                    tvShanghaiWelcome.setVisibility(View.INVISIBLE);
+                }else{
+                    tvShanghaiWelcome.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
     }
 }
